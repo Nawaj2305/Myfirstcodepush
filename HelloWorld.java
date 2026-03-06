ecbd961a173dc99094d77d639059bf5b64366bb4
Nawaj2305 <nawajshaikh23@gmail.com>
2026-03-06 18:20:42 +0530

    Rename HelloWorld class to Simple

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,4 +1,5 @@
-public class HelloWorld {
+public class Simple
+    {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); // Prints the message to the console
     }
